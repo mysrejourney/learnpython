681d32deb433cc5b939b35cdde5197d9d08243ac
Satheesh Pandian <satheeshpandian.j@idfcbank.com>
2023-02-14 15:17:35 +0530

    Day 1 - Band Name Generator Project and its supportive learning files

---
+++ Day 1/band_generator.py
@@ -0,0 +1,9 @@
+# Print Welcome message
+print("Welcome to the Band Name Generator.")
+# Ask the input of the born city
+city_name = input("What's name of the city you grew up in? \n")
+# Ask the input of the pet name
+pet_name = input("What's your pet's name? \n")
+# print output of band name
+print("Your band name could be " + city_name + " "+ pet_name)
+
