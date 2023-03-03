f5380eadb1d32d0a50ad7452602b39efdcd9be06
Satheesh Pandian <satheeshpandian.j@idfcbank.com>
2023-03-03 15:46:24 +0530

    Day 9 - Blind auction project and its supportive learning files

---
+++ Day 9/practice.py
@@ -0,0 +1,26 @@
+# starting_dictionary = {
+#     "a": 9,
+#     "b": 8,
+# }
+#
+#
+# final_dictionary = {
+#     "a": 9,
+#     "b": 8,
+#     "c": 7,
+# }
+# starting_dictionary["C"] = 7
+# final_dictionary = starting_dictionary
+# print(final_dictionary)
+
+dict = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+print(dict[1])
+# for key in dict:
+#     dict[key] += 1
+# dict["c"] = [1,2,3]
+dict[1] = 4
+print(dict)
